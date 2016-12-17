e144800bbe2cd06b5aaa9496d10d87adc8f882bd
mr.xie <393197906@qq.com>
2016-12-18 01:22:22 +0800

    da 增加扩展功能

--- da/index.js
+++ da/index.js
@@ -1,26 +1,42 @@
-window.onload = function(){
-    console.log(da("h1").html());
+window.onload = function() {
+    da.extend("haha", function() {
+         console.log(this.innerText);
+    })
+
+    console.dir(da("h1"));
+
 }
 var da = (function() {
+    var exten = { //扩展用对象
+
+    };
+
     function core(select) {
         var dom = document.querySelector(select);
         var $fn = new fn();
-        $fn.__proto__ = dom.__proto__;
+        exten.__proto__ = dom.__proto__;
+        $fn.__proto__ = exten;
         dom.__proto__ = $fn;
         return dom;
 
     }
 
+    core.extend = function(name, fn) { //扩展方法
+        exten[name] = fn;
+    }
+
+
+
     function fn() {
         this.text = function() {
-            if(arguments.length ==0){
+            if (arguments.length == 0) {
                 return this.innerText;
             }
             this.innerText = arguments[0];
         }
 
-        this.html = function(){
-            if(arguments.length ==0){
+        this.html = function() {
+            if (arguments.length == 0) {
                 return this.innerHTML;
             }
             this.innerHTML = arguments[0];
